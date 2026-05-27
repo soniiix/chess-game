030de0865c753a37cf984b162190dd17d977db32
soniiix <quent.couzinet@gmail.com>
2026-05-27 12:03:47 +0200

    feat: add PieceFactory

---
+++ src/Factory/PieceFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+class Factory {
+    
+    public function create(PieceType $type, PieceColor $color, Position $position): Piece {
+        return match ($type) {
+            PieceType::KING => new King($color, $position),
+            PieceType::QUEEN => new Queen($color, $position),
+            PieceType::ROOK => new Rook($color, $position),
+            PieceType::BISHOP => new Bishop($color, $position),
+            PieceType::KNIGHT => new Knight($color, $position),
+            PieceType::PAWN => new Pawn($color, $position)
+        };
+    }
+}
